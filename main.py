404eee5a83ff49801c2890cb155b815be2ebe9e9
Maaz Ahmed <93466155+CodewithMaaz@users.noreply.github.com>
2022-01-20 15:31:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Drawing a triangle ")
+from colorama import Fore, Back, Style
+print(Fore.GREEN + 'Made By code with maaz')
+print(Fore.BLUE + '   / | ')
+print(Fore.BLUE + '  /  | ')
+print(Fore.BLUE + ' /   | ')
+print(Fore.BLUE + '/____| ')
+print(Fore.RED + 'Thank you for using me')
+print(Style.RESET_ALL)  
